643c4accf302197ff879bfe5e60315bd2da9bc79
seungyeopme <seungyeop.me.dev@gmail.com>
2020-10-22 19:01:25 +0900

    Spring

--- BoardWeb/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
@@ -5,7 +5,7 @@ import org.aspectj.lang.JoinPoint;
 public class AfterThrowingAdvice {
 	public void exceptionLog(JoinPoint jp, Exception exceptObj) {
 		String method = jp.getSignature().getName();
-		System.out.println(method + "() 메소드 수행 중 예외 발생!");
+//		System.out.println(method + "() 메소드 수행 중 예외 발생!");
 		
 		if(exceptObj instanceof IllegalArgumentException) {
 			System.out.println("부적합한 값이 입력되었습니다.");
