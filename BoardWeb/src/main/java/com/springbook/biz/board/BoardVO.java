bd997840948e8882b23a17a37a4e483420f2a542
seungyeopme <seungyeop.me.dev@gmail.com>
2020-10-29 15:41:01 +0900

    Spring

--- BoardWeb/src/main/java/com/springbook/biz/board/BoardVO.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/BoardVO.java
@@ -1,19 +1,29 @@
 package com.springbook.biz.board;
 
-import java.sql.Date;
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlTransient;
 
 import org.springframework.web.multipart.MultipartFile;
 
 //VO(Value Object)
+@XmlAccessorType(XmlAccessType.FIELD)
 public class BoardVO {
+	@XmlAttribute
 	private int seq;
 	private String title;
 	private String writer;
 	private String content;
 	private Date regDate;
 	private int cnt;
+	@XmlTransient
 	private String searchCondition;
+	@XmlTransient
 	private String searchKeyword;
+	@XmlTransient
 	private MultipartFile uploadFile;
 
 	public int getSeq() {
@@ -64,6 +74,7 @@ public class BoardVO {
 		this.cnt = cnt;
 	}
 
+//	@JsonIgnore
 	public String getSearchCondition() {
 		return searchCondition;
 	}
@@ -71,7 +82,8 @@ public class BoardVO {
 	public void setSearchCondition(String searchCondition) {
 		this.searchCondition = searchCondition;
 	}
-
+	
+//	@JsonIgnore
 	public String getSearchKeyword() {
 		return searchKeyword;
 	}
@@ -79,7 +91,8 @@ public class BoardVO {
 	public void setSearchKeyword(String searchKeyword) {
 		this.searchKeyword = searchKeyword;
 	}	
-
+	
+//	@JsonIgnore
 	public MultipartFile getUploadFile() {
 		return uploadFile;
 	}
@@ -94,6 +107,203 @@ public class BoardVO {
 				+ regDate + ", cnt=" + cnt + "]";
 	}
 }
+//package com.springbook.biz.board;
+//
+//import java.sql.Date;
+//
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//
+////VO(Value Object)
+//public class BoardVO {
+//	private int seq;
+//	private String title;
+//	private String writer;
+//	private String content;
+//	private Date regDate;
+//	private int cnt;
+//	private String searchCondition;
+//	private String searchKeyword;
+//	private MultipartFile uploadFile;
+//
+//	public int getSeq() {
+//		return seq;
+//	}
+//
+//	public void setSeq(int seq) {
+//		this.seq = seq;
+//	}
+//
+//	public String getTitle() {
+//		return title;
+//	}
+//
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//
+//	public String getWriter() {
+//		return writer;
+//	}
+//
+//	public void setWriter(String writer) {
+//		this.writer = writer;
+//	}
+//
+//	public String getContent() {
+//		return content;
+//	}
+//
+//	public void setContent(String content) {
+//		this.content = content;
+//	}
+//
+//	public Date getRegDate() {
+//		return regDate;
+//	}
+//
+//	public void setRegDate(Date regDate) {
+//		this.regDate = regDate;
+//	}
+//
+//	public int getCnt() {
+//		return cnt;
+//	}
+//
+//	public void setCnt(int cnt) {
+//		this.cnt = cnt;
+//	}
+//
+//	@JsonIgnore
+//	public String getSearchCondition() {
+//		return searchCondition;
+//	}
+//
+//	public void setSearchCondition(String searchCondition) {
+//		this.searchCondition = searchCondition;
+//	}
+//	
+//	@JsonIgnore
+//	public String getSearchKeyword() {
+//		return searchKeyword;
+//	}
+//
+//	public void setSearchKeyword(String searchKeyword) {
+//		this.searchKeyword = searchKeyword;
+//	}	
+//	
+//	@JsonIgnore
+//	public MultipartFile getUploadFile() {
+//		return uploadFile;
+//	}
+//
+//	public void setUploadFile(MultipartFile uploadFile) {
+//		this.uploadFile = uploadFile;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "BoardVO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
+//				+ regDate + ", cnt=" + cnt + "]";
+//	}
+//}
+//package com.springbook.biz.board;
+//
+//import java.sql.Date;
+//
+//import org.springframework.web.multipart.MultipartFile;
+//
+////VO(Value Object)
+//public class BoardVO {
+//	private int seq;
+//	private String title;
+//	private String writer;
+//	private String content;
+//	private Date regDate;
+//	private int cnt;
+//	private String searchCondition;
+//	private String searchKeyword;
+//	private MultipartFile uploadFile;
+//
+//	public int getSeq() {
+//		return seq;
+//	}
+//
+//	public void setSeq(int seq) {
+//		this.seq = seq;
+//	}
+//
+//	public String getTitle() {
+//		return title;
+//	}
+//
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//
+//	public String getWriter() {
+//		return writer;
+//	}
+//
+//	public void setWriter(String writer) {
+//		this.writer = writer;
+//	}
+//
+//	public String getContent() {
+//		return content;
+//	}
+//
+//	public void setContent(String content) {
+//		this.content = content;
+//	}
+//
+//	public Date getRegDate() {
+//		return regDate;
+//	}
+//
+//	public void setRegDate(Date regDate) {
+//		this.regDate = regDate;
+//	}
+//
+//	public int getCnt() {
+//		return cnt;
+//	}
+//
+//	public void setCnt(int cnt) {
+//		this.cnt = cnt;
+//	}
+//
+//	public String getSearchCondition() {
+//		return searchCondition;
+//	}
+//
+//	public void setSearchCondition(String searchCondition) {
+//		this.searchCondition = searchCondition;
+//	}
+//
+//	public String getSearchKeyword() {
+//		return searchKeyword;
+//	}
+//
+//	public void setSearchKeyword(String searchKeyword) {
+//		this.searchKeyword = searchKeyword;
+//	}	
+//
+//	public MultipartFile getUploadFile() {
+//		return uploadFile;
+//	}
+//
+//	public void setUploadFile(MultipartFile uploadFile) {
+//		this.uploadFile = uploadFile;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "BoardVO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
+//				+ regDate + ", cnt=" + cnt + "]";
+//	}
+//}
 //package com.springbook.biz.board;
 //
 //import java.sql.Date;
