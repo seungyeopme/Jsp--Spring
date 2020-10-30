c427980679eb003e51557892afeffd074dce2b61
seungyeopme <seungyeop.me.dev@gmail.com>
2020-10-30 10:57:20 +0900

    Spring

--- BoardWeb/src/main/java/com/springbook/biz/board/BoardVO.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/BoardVO.java
@@ -2,28 +2,34 @@ package com.springbook.biz.board;
 
 import java.util.Date;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlTransient;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 import org.springframework.web.multipart.MultipartFile;
 
 //VO(Value Object)
-@XmlAccessorType(XmlAccessType.FIELD)
+@Entity
+@Table(name = "BOARD")
 public class BoardVO {
-	@XmlAttribute
+	@Id
+	@GeneratedValue
 	private int seq;
 	private String title;
 	private String writer;
 	private String content;
+	@Temporal(TemporalType.DATE)
 	private Date regDate;
 	private int cnt;
-	@XmlTransient
+	@Transient
 	private String searchCondition;
-	@XmlTransient
+	@Transient
 	private String searchKeyword;
-	@XmlTransient
+	@Transient
 	private MultipartFile uploadFile;
 
 	public int getSeq() {
@@ -74,7 +80,7 @@ public class BoardVO {
 		this.cnt = cnt;
 	}
 
-//	@JsonIgnore
+	// @JsonIgnore
 	public String getSearchCondition() {
 		return searchCondition;
 	}
@@ -82,17 +88,15 @@ public class BoardVO {
 	public void setSearchCondition(String searchCondition) {
 		this.searchCondition = searchCondition;
 	}
-	
-//	@JsonIgnore
+
 	public String getSearchKeyword() {
 		return searchKeyword;
 	}
 
 	public void setSearchKeyword(String searchKeyword) {
 		this.searchKeyword = searchKeyword;
-	}	
-	
-//	@JsonIgnore
+	}
+
 	public MultipartFile getUploadFile() {
 		return uploadFile;
 	}
@@ -107,6 +111,115 @@ public class BoardVO {
 				+ regDate + ", cnt=" + cnt + "]";
 	}
 }
+//package com.springbook.biz.board;
+//
+//import java.util.Date;
+//
+//import javax.xml.bind.annotation.XmlAccessType;
+//import javax.xml.bind.annotation.XmlAccessorType;
+//import javax.xml.bind.annotation.XmlAttribute;
+//import javax.xml.bind.annotation.XmlTransient;
+//
+//import org.springframework.web.multipart.MultipartFile;
+//
+////VO(Value Object)
+//@XmlAccessorType(XmlAccessType.FIELD)
+//public class BoardVO {
+//	@XmlAttribute
+//	private int seq;
+//	private String title;
+//	private String writer;
+//	private String content;
+//	private Date regDate;
+//	private int cnt;
+//	@XmlTransient
+//	private String searchCondition;
+//	@XmlTransient
+//	private String searchKeyword;
+//	@XmlTransient
+//	private MultipartFile uploadFile;
+//
+//	public int getSeq() {
+//		return seq;
+//	}
+//
+//	public void setSeq(int seq) {
+//		this.seq = seq;
+//	}
+//
+//	public String getTitle() {
+//		return title;
+//	}
+//
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//
+//	public String getWriter() {
+//		return writer;
+//	}
+//
+//	public void setWriter(String writer) {
+//		this.writer = writer;
+//	}
+//
+//	public String getContent() {
+//		return content;
+//	}
+//
+//	public void setContent(String content) {
+//		this.content = content;
+//	}
+//
+//	public Date getRegDate() {
+//		return regDate;
+//	}
+//
+//	public void setRegDate(Date regDate) {
+//		this.regDate = regDate;
+//	}
+//
+//	public int getCnt() {
+//		return cnt;
+//	}
+//
+//	public void setCnt(int cnt) {
+//		this.cnt = cnt;
+//	}
+//
+////	@JsonIgnore
+//	public String getSearchCondition() {
+//		return searchCondition;
+//	}
+//
+//	public void setSearchCondition(String searchCondition) {
+//		this.searchCondition = searchCondition;
+//	}
+//	
+////	@JsonIgnore
+//	public String getSearchKeyword() {
+//		return searchKeyword;
+//	}
+//
+//	public void setSearchKeyword(String searchKeyword) {
+//		this.searchKeyword = searchKeyword;
+//	}	
+//	
+////	@JsonIgnore
+//	public MultipartFile getUploadFile() {
+//		return uploadFile;
+//	}
+//
+//	public void setUploadFile(MultipartFile uploadFile) {
+//		this.uploadFile = uploadFile;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "BoardVO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
+//				+ regDate + ", cnt=" + cnt + "]";
+//	}
+//}
 //package com.springbook.biz.board;
 //
 //import java.sql.Date;
--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -11,12 +11,9 @@ import com.springbook.biz.board.BoardVO;
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
 	@Autowired
-	private BoardDAOMybatis boardDAO;
+	private BoardDAOJPA boardDAO;
 
 	public void insertBoard(BoardVO vo) {
-//		if (vo.getSeq() == 0) {
-//			throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
-//		}
 		boardDAO.insertBoard(vo);
 	}
 
@@ -36,6 +33,44 @@ public class BoardServiceImpl implements BoardService {
 		return boardDAO.getBoardList(vo);
 	}
 }
+//package com.springbook.biz.board.impl;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.springbook.biz.board.BoardService;
+//import com.springbook.biz.board.BoardVO;
+//
+//@Service("boardService")
+//public class BoardServiceImpl implements BoardService {
+//	@Autowired
+//	private BoardDAOMybatis boardDAO;
+//
+//	public void insertBoard(BoardVO vo) {
+////		if (vo.getSeq() == 0) {
+////			throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
+////		}
+//		boardDAO.insertBoard(vo);
+//	}
+//
+//	public void updateBoard(BoardVO vo) {
+//		boardDAO.updateBoard(vo);
+//	}
+//
+//	public void deleteBoard(BoardVO vo) {
+//		boardDAO.deleteBoard(vo);
+//	}
+//
+//	public BoardVO getBoard(BoardVO vo) {
+//		return boardDAO.getBoard(vo);
+//	}
+//
+//	public List<BoardVO> getBoardList(BoardVO vo) {
+//		return boardDAO.getBoardList(vo);
+//	}
+//}
 //package com.springbook.biz.board.impl;
 //
 //import java.util.List;
