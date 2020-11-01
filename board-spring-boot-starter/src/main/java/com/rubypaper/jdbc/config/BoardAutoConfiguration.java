71838c27a032351de7a13585169ec163cfd4a205
seungyeopme <seungyeop.me.dev@gmail.com>
2020-11-01 17:17:18 +0900

    Spring Boot

--- board-spring-boot-starter/src/main/java/com/rubypaper/jdbc/config/BoardAutoConfiguration.java
+++ board-spring-boot-starter/src/main/java/com/rubypaper/jdbc/config/BoardAutoConfiguration.java
@@ -1,23 +1,77 @@
 package com.rubypaper.jdbc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.rubypaper.jdbc.util.JDBCConnectionManager;
 
 @Configuration
+@EnableConfigurationProperties(JDBCConnectionManagerProperties.class)
 public class BoardAutoConfiguration {
+	@Autowired
+	private JDBCConnectionManagerProperties properties;
 	
 	@Bean
+	@ConditionalOnMissingBean
 	public JDBCConnectionManager getJDBCConnectionManager() {
 		JDBCConnectionManager manager = new JDBCConnectionManager();
 		
-		manager.setDriverClass("oracle.jdbc.driver.OracleDriver");
-		manager.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
-		
-		manager.setUsername("hr");
-		manager.setPassword("hr");
+		manager.setDriverClass(properties.getDriverClass());
+		manager.setUrl(properties.getUrl());
+		manager.setUsername(properties.getUsername());
+		manager.setPassword(properties.getPassword());
 		
 		return manager;
 	}
-} 
+}
+//package com.rubypaper.jdbc.config;
+//
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.rubypaper.jdbc.util.JDBCConnectionManager;
+//
+//@Configuration
+//public class BoardAutoConfiguration {
+//	
+//	@Bean
+//	@ConditionalOnMissingBean
+//	public JDBCConnectionManager getJDBCConnectionManager() {
+//		JDBCConnectionManager manager = new JDBCConnectionManager();
+//		
+//		manager.setDriverClass("oracle.jdbc.driver.OracleDriver");
+//		manager.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+//		
+//		manager.setUsername("hr");
+//		manager.setPassword("hr");
+//		
+//		return manager;
+//	}
+//} 
+//package com.rubypaper.jdbc.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.rubypaper.jdbc.util.JDBCConnectionManager;
+//
+//@Configuration
+//public class BoardAutoConfiguration {
+//	
+//	@Bean
+//	public JDBCConnectionManager getJDBCConnectionManager() {
+//		JDBCConnectionManager manager = new JDBCConnectionManager();
+//		
+//		manager.setDriverClass("oracle.jdbc.driver.OracleDriver");
+//		manager.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+//		
+//		manager.setUsername("hr");
+//		manager.setPassword("hr");
+//		
+//		return manager;
+//	}
+//}
