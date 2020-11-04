e2d59dd33ddeda49455bebde1748142006b806e4
seungyeopme <seungyeop.me.dev@gmail.com>
2020-11-04 11:41:02 +0900

    Spring Boot

--- Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -1,17 +1,127 @@
-/*페이징과 정렬 처리하기*/
-/*페이징 처리*/
+/*@Query 어노테이션 사용하기*/
+/*/src/test/java/com/rubypaper/QueryAnnotationTest에서 실행*/
+/*페이징 및 정렬 처리하기*/
 package com.rubypaper.persistence;
 
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.rubypaper.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
-	List<Board> findByTitleContaining(String searchKeyword, Pageable paging);
+	@Query("SELECT b FROM Board b ORDER BY b.seq DESC")
+	List<Board> queryAnnotationTest4(Pageable paging);
 }
+
+/*@Query 어노테이션 사용하기*/
+/*/src/test/java/com/rubypaper/QueryAnnotationTest에서 실행*/
+/*네이티브 쿼리 사용하기*/
+//package com.rubypaper.persistence;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.rubypaper.domain.Board;
+//
+//public interface BoardRepository extends CrudRepository<Board, Long> {
+//	@Query(value="select seq, title, writer, create_date "
+//          + "from board where title like '%'||?1||'%' "
+//		  + "order by seq desc", nativeQuery=true)
+//	List<Object[]> queryAnnotationTest3(String searchKeyword);
+//}
+
+/*@Query 어노테이션 사용하기*/
+/*/src/test/java/com/rubypaper/QueryAnnotationTest에서 실행*/
+/*특정 변수만 조회하기*/
+//package com.rubypaper.persistence;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.query.Param;
+//
+//import com.rubypaper.domain.Board;
+//
+//public interface BoardRepository extends CrudRepository<Board, Long> {
+//	
+//	@Query("SELECT b.seq, b.title, b.writer, b.createDate "
+//			+ "FROM Board b "
+//			+ "WHERE b.title like %?1% "
+//			+ "ORDER BY b.seq DESC")
+//	List<Object[]> queryAnnotationTest2(@Param("searchKeyword") String searchKeyword);
+//}
+
+/*@Query 어노테이션 사용하기*/
+/*/src/test/java/com/rubypaper/QueryAnnotationTest에서 실행*/
+/*Query 어노테이션*/
+/*이름 기반 파라미터 사용하기*/
+//package com.rubypaper.persistence;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.query.Param;
+//
+//import com.rubypaper.domain.Board;
+//
+//public interface BoardRepository extends CrudRepository<Board, Long> {
+//	
+//	@Query("SELECT b FROM Board b WHERE b.title like %:searchKeyword% ORDER BY b.seq DESC")
+//	List<Board> queryAnnotationTest1(@Param("searchKeyword") String searchKeyword);
+//}
+
+/*@Query 어노테이션 사용하기*/
+/*/src/test/java/com/rubypaper/QueryAnnotationTest에서 실행*/
+/*Query 어노테이션*/
+//package com.rubypaper.persistence;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.rubypaper.domain.Board;
+//
+//public interface BoardRepository extends CrudRepository<Board, Long> {
+//	
+//	@Query("SELECT b FROM Board b WHERE b.title like %?1% ORDER BY b.seq DESC")
+//	List<Board> queryAnnotationTest1(String searchKeyword);
+//}
+
+/*Page<T>타입 사용하기*/
+//package com.rubypaper.persistence;
+//
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.rubypaper.domain.Board;
+//
+//public interface BoardRepository extends CrudRepository<Board, Long> {
+//	Page<Board> findByTitleContaining(String searchKeyword, Pageable paging);
+//}
+
+/*페이징과 정렬 처리하기*/
+/*페이징 처리*/
+//package com.rubypaper.persistence;
+//
+//import java.util.List;
+//
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.rubypaper.domain.Board;
+//
+//public interface BoardRepository extends CrudRepository<Board, Long> {
+//	List<Board> findByTitleContaining(String searchKeyword, Pageable paging);
+//}
 
 
 /*데이터 정렬하기*/
--- Chapter05/src/test/java/com/rubypaper/QueryMethodTest.java
+++ Chapter05/src/test/java/com/rubypaper/QueryMethodTest.java
@@ -1,54 +1,98 @@
+/*Page<T>타입 사용하기*/
+//package com.rubypaper;
+//
+//import java.util.List;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.domain.Sort;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import com.rubypaper.domain.Board;
+//import com.rubypaper.persistence.BoardRepository;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class QueryMethodTest {
+//	@Autowired
+//	private BoardRepository boardRepo;
+//
+//	@Test
+//	public void testFindByTitleContaining() {
+//		Pageable paging = PageRequest.of(0, 5, Sort.Direction.DESC, "seq");
+//		
+//		Page<Board> pageInfo = boardRepo.findByTitleContaining("제목", paging);
+//		
+//		System.out.println("PAGE SIZE : " + pageInfo.getSize());
+//		System.out.println("TOTAL PAGES : " + pageInfo.getTotalPages());
+//		System.out.println("TOTAL COUNT : " + pageInfo.getTotalElements());
+//		System.out.println("NEXT : " + pageInfo.nextPageable());
+//		
+//		List<Board> boardList = pageInfo.getContent();
+//		
+//		System.out.println("검색 결과");
+//		for (Board board : boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+//}
+
 /*페이징과 정렬 처리하기*/
 /*정렬 처리*/
-package com.rubypaper;
-
-import java.util.Date;
-import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.rubypaper.domain.Board;
-import com.rubypaper.persistence.BoardRepository;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class QueryMethodTest {
-	@Autowired
-	private BoardRepository boardRepo;
-	
-	@Before
-	public void dataPrepare() {
-		for (int i = 1; i <= 200; i++) {
-			Board board = new Board();
-			board.setTitle("테스트 제목 " + i);
-			board.setWriter("테스터");
-			board.setContent("테스트 내용 " + i);			
-			board.setCreateDate(new Date());	
-			board.setCnt(0L);
-			boardRepo.save(board);
-		}
-	}
-	
-	@Test
-	public void testFindByTitleContaining() {
-		Pageable paging = PageRequest.of(0, 5, Sort.Direction.DESC, "seq");
-		
-		List<Board> boardList = boardRepo.findByTitleContaining("제목", paging);
-		
-		System.out.println("검색 결과");
-		for (Board board : boardList) {
-			System.out.println("---> " + board.toString());
-		}
-	}
-}
+//package com.rubypaper;
+//
+//import java.util.Date;
+//import java.util.List;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.domain.Sort;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import com.rubypaper.domain.Board;
+//import com.rubypaper.persistence.BoardRepository;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class QueryMethodTest {
+//	@Autowired
+//	private BoardRepository boardRepo;
+//	
+//	@Before
+//	public void dataPrepare() {
+//		for (int i = 1; i <= 200; i++) {
+//			Board board = new Board();
+//			board.setTitle("테스트 제목 " + i);
+//			board.setWriter("테스터");
+//			board.setContent("테스트 내용 " + i);			
+//			board.setCreateDate(new Date());	
+//			board.setCnt(0L);
+//			boardRepo.save(board);
+//		}
+//	}
+//	
+//	@Test
+//	public void testFindByTitleContaining() {
+//		Pageable paging = PageRequest.of(0, 5, Sort.Direction.DESC, "seq");
+//		
+//		List<Board> boardList = boardRepo.findByTitleContaining("제목", paging);
+//		
+//		System.out.println("검색 결과");
+//		for (Board board : boardList) {
+//			System.out.println("---> " + board.toString());
+//		}
+//	}
+//}
 
 /*페이징과 정렬 처리하기*/
 /*페이징 처리*/
